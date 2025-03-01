206cbbd175c57305ea1214c1693456ee56d78261
SUCHI <svp@udel.edu>
2025-03-01 15:08:05 -0500

    checking

--- src/HomePage/HomePage.tsx
+++ src/HomePage/HomePage.tsx
@@ -5,7 +5,6 @@ const HomePage: React.FC = () => {
   return (
     <div className="homepage">
       <h1>Welcome to GainsAI</h1>
-      <p>This is the homepage for our project.</p>
     </div>
   );
 };
