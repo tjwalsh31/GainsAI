14e091e95ee005872de4afdcef5b1a18cf43465e
Tyler Walsh <tjwalsh@udel.edu>
2025-03-01 16:12:25 -0500

    Started nav bar, when deployed should render HomePage.js

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/" className="navbar-logo">
+                GainsAI
+            </Link>
+            <ul className="navbar-links">
+                <li className="navbar-link">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="navbar-link">
+                    <Link to="/workout">Workout Planner</Link>
+                </li>
+                <li className="navbar-link">
+                    <Link to="/meal">Meal Planner</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar"; // Import Navbar component
 import "./HomePage.css"; // Import CSS for styling
 
 const HomePage = () => {
   return (
     <div className="homepage">
+      <Navbar /> {/* Add Navbar component */}
       <h1>Welcome to GainsAI</h1>
     </div>
   );
